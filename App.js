3f0c1225c44604b5eae39b9f9d6821d21bea68f3
Alex Hint <alex.hint@betterment.com>
2017-12-03 00:07:59 -0500

    Add inputs and calculate the diff

--- App.js
+++ App.js
@@ -1,34 +1,72 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      BTCPrice: 0
+      currentBTCPrice: '0',
+      purchasedBTCAmount: '0',
+      purchasedBTCPrice: '0',
     }
   }
 
   componentDidMount() {
-    this.fetchBTCPrice()
+    this.fetchCurrentBTCPrice()
   }
 
-  fetchBTCPrice() {
+  fetchCurrentBTCPrice() {
     return fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
       .then(response => response.json())
       .then(responseJson => {
-        this.setState({ BTCPrice: responseJson['bpi']['USD']['rate'] });
+        this.setState({ currentBTCPrice: parseFloat(responseJson['bpi']['USD']['rate_float']).toFixed(2) });
       })
       .catch(error => {
         console.error(error);
       });
   }
 
+  calculateDiff() {
+    return (
+      (
+        parseFloat(this.state.currentBTCPrice).toFixed(2) - parseFloat(this.state.purchasedBTCPrice
+      ).toFixed(2)) * this.state.purchasedBTCAmount
+    ).toFixed(2);
+  }
+
+  updateAmountInput(purchasedBTCAmount) {
+    this.setState({ purchasedBTCAmount: purchasedBTCAmount })
+  }
+
+  updatePriceInput(purchasedBTCPrice) {
+    this.setState({ purchasedBTCPrice: purchasedBTCPrice})
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Price of BTC is:</Text>
-        <Text>${ this.state.BTCPrice }</Text>
+        <Text>${ this.state.currentBTCPrice }</Text>
+
+        <Text>How many BTC did you purchase?</Text>
+        <TextInput
+          style={styles.input}
+          keyboardType='numeric'
+          placeholder='0.00'
+          value={ this.state.purchasedBTCAmount }
+          onChangeText={ input => this.updateAmountInput(input) }
+        />
+        <Text>At what price $$$?</Text>
+        <TextInput
+          style={styles.input}
+          keyboardType='numeric'
+          placeholder='0.00'
+          value={ this.state.purchasedBTCPrice }
+          onChangeText={ input => this.updatePriceInput(input) }
+        />
+
+        <Text>You are up: </Text>
+        <Text>${ this.calculateDiff() }</Text>
       </View>
     );
   }
@@ -40,5 +78,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 50,
   },
+  input: {
+    borderColor: '#000',
+    borderWidth: 1,
+    height: 50,
+    width: '100%',
+    padding: 10,
+  }
 });
