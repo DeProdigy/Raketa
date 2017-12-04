03c1e90241bf7a09fb8dcc0dd6e8512f28b885d2
Alex Hint <alex.hint@betterment.com>
2017-12-03 19:31:39 -0500

    Add multiple screens with navigation

--- App.js
+++ App.js
@@ -1,90 +1,21 @@
-import React from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentBTCPrice: '0',
-      purchasedBTCAmount: '0',
-      purchasedBTCPrice: '0',
-    }
-  }
+import HomeScreen from './HomeScreen';
+import InputsScreen from './InputsScreen';
 
-  componentDidMount() {
-    this.fetchCurrentBTCPrice()
-  }
-
-  fetchCurrentBTCPrice() {
-    return fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
-      .then(response => response.json())
-      .then(responseJson => {
-        this.setState({ currentBTCPrice: parseFloat(responseJson['bpi']['USD']['rate_float']).toFixed(2) });
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }
-
-  calculateDiff() {
-    return (
-      (
-        parseFloat(this.state.currentBTCPrice).toFixed(2) - parseFloat(this.state.purchasedBTCPrice
-      ).toFixed(2)) * this.state.purchasedBTCAmount
-    ).toFixed(2);
-  }
-
-  updateAmountInput(purchasedBTCAmount) {
-    this.setState({ purchasedBTCAmount: purchasedBTCAmount })
-  }
-
-  updatePriceInput(purchasedBTCPrice) {
-    this.setState({ purchasedBTCPrice: purchasedBTCPrice})
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Price of BTC is:</Text>
-        <Text>${ this.state.currentBTCPrice }</Text>
-
-        <Text>How many BTC did you purchase?</Text>
-        <TextInput
-          style={styles.input}
-          keyboardType='numeric'
-          placeholder='0.00'
-          value={ this.state.purchasedBTCAmount }
-          onChangeText={ input => this.updateAmountInput(input) }
-        />
-        <Text>At what price $$$?</Text>
-        <TextInput
-          style={styles.input}
-          keyboardType='numeric'
-          placeholder='0.00'
-          value={ this.state.purchasedBTCPrice }
-          onChangeText={ input => this.updatePriceInput(input) }
-        />
-
-        <Text>You are up: </Text>
-        <Text>${ this.calculateDiff() }</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 50,
+const RootNavigator = StackNavigator({
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: {
+      headerTitle: 'Home',
+    },
   },
-  input: {
-    borderColor: '#000',
-    borderWidth: 1,
-    height: 50,
-    width: '100%',
-    padding: 10,
-  }
+  Inputs: {
+    screen: InputsScreen,
+    navigationOptions: {
+      headerTitle: 'Inputs',
+    },
+  },
 });
+
+export default RootNavigator;
---
+++ HomeScreen.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
+
+export default class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentBTCPrice: '0',
+      purchasedBTCAmount: '0',
+      purchasedBTCPrice: '0',
+    }
+  }
+
+  static navigationOptions = {
+    title: 'Welcome',
+  };
+
+  componentDidMount() {
+    this.fetchCurrentBTCPrice()
+  }
+
+  fetchCurrentBTCPrice() {
+    return fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({ currentBTCPrice: parseFloat(responseJson['bpi']['USD']['rate_float']).toFixed(2) });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  calculateDiff() {
+    return (
+      (
+        parseFloat(this.state.currentBTCPrice).toFixed(2) -
+        parseFloat(this.state.purchasedBTCPrice
+      ).toFixed(2)) * this.state.purchasedBTCAmount
+    ).toFixed(2);
+  }
+
+  updateAmountInput(purchasedBTCAmount) {
+    this.setState({ purchasedBTCAmount: purchasedBTCAmount })
+  }
+
+  updatePriceInput(purchasedBTCPrice) {
+    this.setState({ purchasedBTCPrice: purchasedBTCPrice})
+  }
+
+  render() {
+    const { navigate } = this.props.navigation;
+
+    return (
+      <View style={styles.container}>
+        <Text>Price of BTC is:</Text>
+        <Text>${ this.state.currentBTCPrice }</Text>
+
+        <Text>How many BTC did you purchase?</Text>
+        <TextInput
+          style={styles.input}
+          keyboardType='numeric'
+          placeholder='0.00'
+          value={ this.state.purchasedBTCAmount }
+          onChangeText={ input => this.updateAmountInput(input) }
+        />
+        <Text>At what price $$$?</Text>
+        <TextInput
+          style={styles.input}
+          keyboardType='numeric'
+          placeholder='0.00'
+          value={ this.state.purchasedBTCPrice }
+          onChangeText={ input => this.updatePriceInput(input) }
+        />
+
+        <Text>You are up: </Text>
+        <Text>${ this.calculateDiff() }</Text>
+
+
+        <Button
+          title="Inputs"
+          onPress={ () => navigate('Inputs') }
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 50,
+  },
+  input: {
+    borderColor: '#000',
+    borderWidth: 1,
+    height: 50,
+    width: '100%',
+    padding: 10,
+  }
+});
