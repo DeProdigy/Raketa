5f4705047f1ee96c3276a5c77d9ec00386901979
Alex Hint <alex.hint@betterment.com>
2017-12-16 17:50:21 -0500

    Display a list of TOP 100 coins

--- App.js
+++ App.js
@@ -1,41 +1,95 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, Button, View, FlatList } from 'react-native';
+import { StyleSheet, View, Text, FlatList, Image } from 'react-native';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      cryptoCompareDataFetched: false,
+      coinMarketcapDataFetched: false,
+      cryptoCompareData: {},
+      coinMarketcapData: [],
+    }
   }
 
   componentDidMount() {
-    this.fetchCurrentBTCPrice()
+    this.fetchCryptoCompareData();
+    this.fetchCoinMarketData();
   }
 
-  fetchCurrentBTCPrice() {
-    return fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+  fetchCryptoCompareData() {
+    return fetch('https://www.cryptocompare.com/api/data/coinlist/')
       .then(response => response.json())
       .then(responseJson => {
-        this.setState({ currentBTCPrice: parseFloat(responseJson['bpi']['USD']['rate_float']).toFixed(2) });
+        this.setState({ cryptoCompareData: responseJson, cryptoCompareDataFetched: true });
       })
       .catch(error => {
         console.error(error);
       });
   }
 
+  fetchCoinMarketData() {
+    return fetch('https://api.coinmarketcap.com/v1/ticker/?limit=100')
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({ coinMarketcapData: responseJson, coinMarketcapDataFetched: true });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  coinImage(coin) {
+    if (coin.symbol == 'MIOTA') {
+      return 'https://www.cryptocompare.com/media/1383540/iota_logo.png';
+    } else if (coin.symbol == 'BCC') {
+      return 'https://www.cryptocompare.com/media/9350709/bccoin1.png';
+    } else if (this.state.cryptoCompareData['Data'][coin.symbol]) {
+      return 'https://www.cryptocompare.com' + this.state.cryptoCompareData['Data'][coin.symbol]['ImageUrl'];
+    } else {
+      return 'https://www.moneymetals.com/images/products/10-dollar-liberty-gold-coin-20140321124620.jpg';
+    }
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-          <Text>You are here</Text>
-      </View>
-    );
+    if (this.state.cryptoCompareDataFetched && this.state.coinMarketcapDataFetched) {
+      return (
+          <View style={styles.container}>
+              <FlatList
+                  data={this.state.coinMarketcapData}
+                  renderItem={({item}) =>
+                      <View style={styles.listCell}>
+                          <Image
+                              style={{width: 10, height: 10}}
+                              source={{uri: this.coinImage(item)}}
+                          />
+                          <Text>
+                              {item.symbol} ${item.price_usd}
+                          </Text>
+                      </View>
+                  }
+                  keyExtractor={(item, index) => index}
+              />
+          </View>
+      );
+    } else {
+      return (
+          <View style={styles.container}>
+              <Text>Loading...</Text>
+          </View>
+      );
+    }
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
     padding: 50,
   },
+  listCell: {
+    width: '100%',
+    height: 30,
+  }
 });
