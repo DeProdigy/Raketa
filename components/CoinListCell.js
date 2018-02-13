0f2140a7bbbb8b96c83a51be45c9cb909c4a613d
Daniel Rak <danielrak@me.com>
2018-02-13 00:51:52 -0500

    style cells (not 100% done)

--- components/CoinListCell.js
+++ components/CoinListCell.js
@@ -29,32 +29,44 @@ export default class CoinListCell extends Component {
 
   render() {
 
-    const isPercentChangePositive = this.state.item.percent_change_24h > 0;
-    return (
-      <View style={coinListCellStyles.listCell}>
+  const isPercentChangeNegative = this.state.item.percent_change_24h < 0;
+
+  return (
+    <View style={coinListCellStyles.mainContainer}>
+      <View style={coinListCellStyles.nameSymbolContainer}>
         <Image
-          source={isPercentChangePositive ?
-            require('../images/icon-up-change.png') :
-            require('../images/icon-down-change.png')
-        }
-        />
-        <Image
-          style={coinListCellStyles.listCellImage}
+          style={coinListCellStyles.symbolImage}
           source={{uri: this.coinImage(this.state.item)}}
         />
-        <Text style={coinListCellStyles.listCellInfo}>
-          {this.state.item.symbol}
-        </Text>
-        <Text style={coinListCellStyles.listCellInfo}>
-          {this.state.item.name}
-        </Text>
-        <Text style={coinListCellStyles.listCellInfo}>
-          ${this.state.item.price_usd}
-        </Text>
-        <Text style={coinListCellStyles.listCellInfo}>
-          {this.state.item.percent_change_24h}%
-        </Text>
+        <View style={coinListCellStyles.nameContainer}>
+          <Text style={coinListCellStyles.nameText}>
+            {this.state.item.name}
+          </Text>
+          <Text style={coinListCellStyles.symbolText}>
+            {this.state.item.symbol}
+          </Text>
+        </View>
       </View>
+      <View style={coinListCellStyles.priceContainer}>
+        <Text style={coinListCellStyles.priceText}>
+            ${this.state.item.price_usd}
+        </Text>
+        <View style={
+          isPercentChangeNegative ?
+          [coinListCellStyles.changeContainer,coinListCellStyles.changeContainerNegative]:
+          coinListCellStyles.changeContainer
+        }
+        >
+          <Text style={
+            isPercentChangeNegative ?
+            [coinListCellStyles.changeText, coinListCellStyles.changeTextNegative]:
+            coinListCellStyles.changeText}
+          >
+            {this.state.item.percent_change_24h}%
+          </Text>
+        </View>
+      </View>
+    </View>
     );
   }
 }
