02a6b077eb99a5ed14304c2e4e6e9ba26265bc2f
Daniel Rak <danielrak@me.com>
2018-02-28 02:25:23 -0500

    CoinListCellChangeView: destructure props

--- app/components/CoinListCell/CoinListCellChangeView/CoinListCellChangeView.js
+++ app/components/CoinListCell/CoinListCellChangeView/CoinListCellChangeView.js
@@ -2,31 +2,43 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import coinListCellChangeViewStyles from './CoinListCellChangeViewStyles'
 import CoinListCellChangeButton from '../CoinListCellChangeButton/CoinListCellChangeButton'
+import formulaHelper from '../../../shared/utils/formulaHelper'
+
 
 const s = coinListCellChangeViewStyles
 
-const CoinListCellChangeView = props => (
+const CoinListCellChangeView = ({
+  item: {
+    percent_change_1h: percentChange1h,
+    percent_change_24h: percentChange24h,
+    percent_change_7d: percentChange7d,
+    price_usd: priceUSD,
+  },
+}) => (
   <View style={s.container}>
     <View style={s.buttonsContainer}>
       <View style={s.buttonContainer}>
         <Text style={s.buttonLabelText}>1H</Text>
         <CoinListCellChangeButton
-          isNegative
-          percentChange="19.25"
+          isNegative={percentChange1h < 0}
+          percentChange={percentChange1h}
+          priceChange={formulaHelper.getChangeAmount(priceUSD, percentChange1h)}
         />
       </View>
       <View style={s.buttonContainer}>
         <Text style={s.buttonLabelText}>24H</Text>
         <CoinListCellChangeButton
-          isNegative
-          percentChange="19.25"
+          isNegative={percentChange24h < 0}
+          percentChange={percentChange24h}
+          priceChange={formulaHelper.getChangeAmount(priceUSD, percentChange24h)}
         />
       </View>
       <View style={s.buttonContainer}>
         <Text style={s.buttonLabelText}>7D</Text>
         <CoinListCellChangeButton
-          isNegative
-          percentChange="19.25"
+          isNegative={percentChange7d < 0}
+          percentChange={percentChange7d}
+          priceChange={formulaHelper.getChangeAmount(priceUSD, percentChange7d)}
         />
       </View>
     </View>
