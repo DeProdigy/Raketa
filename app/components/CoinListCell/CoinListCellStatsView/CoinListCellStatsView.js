ba02b15e03fa8d8bf0609696710f1a6db8e42ffd
Daniel Rak <danielrak@me.com>
2018-02-28 02:23:45 -0500

    CoinListCellStatsView: destructure props

--- app/components/CoinListCell/CoinListCellStatsView/CoinListCellStatsView.js
+++ app/components/CoinListCell/CoinListCellStatsView/CoinListCellStatsView.js
@@ -4,28 +4,33 @@ import coinListCellStatsViewStyles from './CoinListCellStatsViewStyles'
 
 const s = coinListCellStatsViewStyles
 
-
-const CoinListCellStatsView = props => (
-
+const CoinListCellStatsView = ({
+  item: {
+    market_cap_usd: marketCap,
+    '24h_volume_usd': volume24h,
+    available_supply: circulatingSupply,
+    max_supply: maxSupply,
+  },
+}) => (
   [
     <View style={s.container} key={1}>
       <View style={s.leftAlignedStatContainer}>
         <Text style={s.headerText}>MARKET CAP</Text>
-        <Text style={s.statText}>${props.item.market_cap_usd}</Text>
+        <Text style={s.statText}>${marketCap}</Text>
       </View>
       <View style={s.rightAlignedStatContainer}>
         <Text style={s.headerText}>VOLUME(24H)</Text>
-        <Text style={[s.statText, s.statTextRight]}>${props.item['24h_volume_usd']}</Text>
+        <Text style={[s.statText, s.statTextRight]}>${volume24h}</Text>
       </View>
     </View>,
     <View style={s.container} key={2}>
       <View style={s.leftAlignedStatContainer}>
         <Text style={s.headerText}>CIRCULATING SUPPLY</Text>
-        <Text style={s.statText}>{props.item.available_supply}</Text>
+        <Text style={s.statText}>{circulatingSupply}</Text>
       </View>
       <View style={s.rightAlignedStatContainer}>
         <Text style={s.headerText}>MAX SUPPLY</Text>
-        <Text style={[s.statText, s.statTextRight]}>{props.item.max_supply}</Text>
+        <Text style={[s.statText, s.statTextRight]}>{maxSupply}</Text>
       </View>
     </View>,
   ]
