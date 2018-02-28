c350040f7e00976667a770847ca56fc0c63aa3ed
Alex Hint <alex.hint@betterment.com>
2018-02-27 21:42:58 -0500

    CoinMarketcapDataFetcher

--- app/screens/Top100Screen/Top100Screen.js
+++ app/screens/Top100Screen/Top100Screen.js
@@ -2,30 +2,16 @@ import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 import top100ScreenStyles from './Top100ScreenStyles'
 import CoinList from '../../components/CoinList/CoinList'
+import coinMarketcapDataFetcher from '../../networkers/coinMarketcapDataFetcher'
 
 export default class Top100Screen extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      coinMarketcapDataFetched: false,
-      coinMarketcapData: [],
-    }
+  state = {
+    coinMarketcapDataFetched: false,
+    coinMarketcapData: [],
   }
 
   componentDidMount() {
-    this.fetchCoinMarketData()
-  }
-
-  fetchCoinMarketData() {
-    return fetch('https://api.coinmarketcap.com/v1/ticker/?limit=100')
-      .then(response => response.json())
-      .then((responseJson) => {
-        this.setState({ coinMarketcapData: responseJson, coinMarketcapDataFetched: true })
-      })
-      .catch((error) => {
-        console.error(error)
-      })
+    coinMarketcapDataFetcher(this)
   }
 
   render() {
